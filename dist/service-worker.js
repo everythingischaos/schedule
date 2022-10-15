313f71d14258f3af70ceee21a1f6b9f1e13c9b80
piguyisme <piguyisme@gmail.com>
2022-10-14 23:07:25 -0700

    Relative paths

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -11,7 +11,15 @@ self.addEventListener("install", (event) => {
       "/style.css",
       "/main.js",
       "/manifest.webmanifest",
-      "/icons/icon-48x48.png"
+      "/icons/icon-48x48.png",
+      "/icons/icon-72x72.png",
+      "/icons/icon-96x96.png",
+      "/icons/icon-128x128.png",
+      "/icons/icon-144x144.png",
+      "/icons/icon-152x152.png",
+      "/icons/icon-192x192.png",
+      "/icons/icon-384x384.png",
+      "/icons/icon-512x512.png",
     ])
   );
 });
