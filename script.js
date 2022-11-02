fcb53c08758c132e5ca449f0bfe2671a1af25dd9
piguyisme <piguyisme@gmail.com>
2022-11-02 08:41:09 -0700

    pwa stuff (incomplete)

--- script.js
+++ script.js
@@ -7,28 +7,28 @@ if (typeof Storage !== 'undefined') {
 }
 
 //Register service worker
-const registerServiceWorker = async () => {
-  if ('serviceWorker' in navigator) {
-    try {
-      const registration = await navigator.serviceWorker.register(
-        './service-worker.js'
-      );
-      if (registration.installing) {
-        console.log('Service worker installing');
-      } else if (registration.waiting) {
-        console.log('Service worker installed');
-      } else if (registration.active) {
-        console.log('Service worker active');
-      }
-    } catch (error) {
-      console.error(`Registration failed with ${error}`);
-    }
-  }
-};
+// const registerServiceWorker = async () => {
+//   if ('serviceWorker' in navigator) {
+//     try {
+//       const registration = await navigator.serviceWorker.register(
+//         './service-worker.js'
+//       );
+//       if (registration.installing) {
+//         console.log('Service worker installing');
+//       } else if (registration.waiting) {
+//         console.log('Service worker installed');
+//       } else if (registration.active) {
+//         console.log('Service worker active');
+//       }
+//     } catch (error) {
+//       console.error(`Registration failed with ${error}`);
+//     }
+//   }
+// };
 
 // …
 
-registerServiceWorker();
+// registerServiceWorker();
 
 //the default schedule
 let defaultAllSchedules = JSON.parse(
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,20 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   base: '',
+  plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,png,svg}'],
+      },
+      devOptions: {
+        enabled: true,
+      }
+    })
+  ],
+  build: {
+    sourcemap: true,
+  }
 })
