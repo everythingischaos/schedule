5247e3b19689cde662652d644453c6f7d8c2d143
Shudfly <35050346+Shudfly@users.noreply.github.com>
2024-12-23 06:45:03 -0800

    gotta do this at some point

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
+import { Clock3Icon, Palette, Sun } from "lucide-react";
 import { useState } from "react";
+import { Button } from "./components/ui/button";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [theme, setTheme] = useState("");
+  const [accent, setAccent] = useState("mauve")
 
   return (
-    <>
-      <div></div>
-    </>
+    <main className={`flex min-h-screen w-full flex-col bg-base ${theme}`}>
+      <div className="flex flex-col">
+        <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-base px-4">
+          <Clock3Icon className="text-text" />
+          <Button className="ml-auto" size="icon" variant="ghost">
+            <Palette />
+          </Button>
+          <Button size="icon" variant="ghost">
+            <Sun />
+          </Button>
+        </header>
+      </div>
+    </main>
   );
 }
 
